f9c7ac29045a3ebe738c90335a17f4b2ff134e79
Ilya Lavrentyev <ilyalavrentyev@gmail.com>
2021-12-19 11:24:20 +0200

    Test

--- Projects/Guess my number/starter/script.js
+++ Projects/Guess my number/starter/script.js
@@ -1,7 +1,13 @@
 'use strict';
 
 document.querySelector('.check').addEventListener('click', () => {
-    alert('😂');
+    const inpNum = document.querySelector('.guess').value;
+    console.log(inpNum, typeof inpNum);
+    document.querySelector('.message').textContent = 'Correct number! 😁';
+
+    if (!inpNum) {
+        document.querySelector('.message').textContent = 'No number! 😒'
+    }
 });
 
 
