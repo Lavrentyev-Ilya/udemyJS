29c2fa98df174bf8d976e9eb4efcbf8ac4c73465
Ilya Lavrentyev <ilyalavrentyev@gmail.com>
2021-12-21 22:44:37 +0200

    Modal

--- Projects/Modal window/script.js
+++ Projects/Modal window/script.js
@@ -1,2 +1,26 @@
 'use strict';
-let qwe = '';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseButton = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+   overlay.classList.remove('hidden');
+   modal.classList.remove('hidden');
+}
+
+const closeModal = function () {
+   overlay.classList.add('hidden');
+   modal.classList.add('hidden');
+}
+for (let i = 0; i < btnsOpenModal.length; i++) {
+   btnsOpenModal[i].addEventListener('click', openModal);
+}
+btnCloseButton.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+     closeModal();
+  }
+});
